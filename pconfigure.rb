c9cc0d055e56c61ccc573202c6153d4e96d9edb3
Palmer Dabbelt <palmer@dabbelt.com>
2013-12-03 19:06:39 -0800

    Add some pconfigure dependencies

--- pconfigure.rb
+++ pconfigure.rb
@@ -5,6 +5,9 @@ class Pconfigure < Formula
   homepage 'https://github.com/palmer-dabbelt/pconfigure/'
   sha1     '5eb5a405b80994c8c2750566560c07c23bf4a2d7'
 
+  depends_on 'pkg-config' => :build
+  depends_on 'talloc'
+
   def install
     system "sed", "-i", "bak", "s/soname/install_name/g", "Configfiles/main"
     system "bash", "-ex", "./bootstrap.sh", "--prefix", "#{prefix}"
