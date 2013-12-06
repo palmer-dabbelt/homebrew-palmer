535cfa016962cda2b8603f9e340de5935d56acd6
Palmer Dabbelt <palmer@dabbelt.com>
2013-12-06 15:51:51 -0800

    Add some pconfigure patches
    This was easier that cutting my own release...

--- pconfigure.rb
+++ pconfigure.rb
@@ -15,4 +15,16 @@ class Pconfigure < Formula
     system "bash", "-ex", "./bootstrap.sh", "--prefix", "#{prefix}"
     system "make", "install"
   end
+
+  def patches
+    [
+     # Use gzip instead of xz for pscalald
+     "https://github.com/palmer-dabbelt/pconfigure/commit/5786df9fc4c066487fe5596aa300bf12578844f5.diff",
+     # Use pkg-config for Chisel pscalald
+     "https://github.com/palmer-dabbelt/pconfigure/commit/34166ff545168c4c809105990911635f4fab7b87.diff",
+     # Use pkg-config for Chisel C++
+     "https://github.com/palmer-dabbelt/pconfigure/commit/48a27100ffd7cda35b92945459d56ed633fce1f3.diff",
+     "https://github.com/palmer-dabbelt/pconfigure/commit/0443be3dcbbc804bd76ecdfbf24fe4132ac15d94.diff"
+    ]
+  end
 end
