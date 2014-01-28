eadef02aba20e0c68306018f1bb3b9b983442d05
Palmer Dabbelt <palmer@dabbelt.com>
2014-01-28 15:20:16 -0800

    Update jrbgui to 0.1.6

--- jrbgui.rb
+++ jrbgui.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Jrbgui < Formula
-  url      'https://github.com/palmer-dabbelt/jrbgui/archive/v0.1.4.tar.gz'
+  url      'https://github.com/palmer-dabbelt/jrbgui/archive/v0.1.6.tar.gz'
   homepage 'https://github.com/palmer-dabbelt/jrbgui'
-  sha1     '9b6f323720c1d7a1e70ca4cc473e5c75960ecbd2'
+  sha1     '539f5c577dc5670dd5772adcc0e4a946169ce5a4'
 
   depends_on 'pkg-config' => :build
   depends_on 'palmer-dabbelt/palmer/pconfigure' => :build
@@ -15,17 +15,6 @@ class Jrbgui < Formula
   depends_on 'palmer-dabbelt/palmer/fakegl'
 
   def install
-    f = File.open("Configfile.local", "w")
-    f.puts("PREFIX = #{prefix}")
-    f.puts("LANGUAGES += c++")
-    f.puts("LINKOPTS += -L#{prefix}/../../../lib")
-    f.puts("LINKOPTS += -framework OpenGL")
-    f.puts("LINKOPTS += -framework GLUT")
-    f.puts("LINKOPTS += -framework Carbon")
-    f.puts("LINKOPTS += -lportmidi")
-    f.puts("LINKOPTS += -lportaudio")
-    f.close
-
     system "pconfigure"
     system "make"
     system "make", "install"
