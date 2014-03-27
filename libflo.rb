0ac3f48ab8dd5441e0e868a9e008a0a3098a75fe
Palmer Dabbelt <palmer@dabbelt.com>
2014-03-27 14:32:38 -0700

    Update libflo to 0.0.4

--- libflo.rb
+++ libflo.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Libflo < Formula
-  url      'https://github.com/palmer-dabbelt/libflo/archive/v0.0.3.tar.gz'
+  url      'https://github.com/palmer-dabbelt/libflo/archive/v0.0.4.tar.gz'
   homepage 'https://github.com/palmer-dabbelt/libflo'
-  sha1     '398617d7de04e971ac9eafed9e16a72acd0798b4'
+  sha1     '0bf5cc7ca853659f3cb800173d8573f9b554266f'
 
   depends_on 'pkg-config' => :build
   depends_on 'palmer-dabbelt/palmer/pconfigure' => :build
