9845ff0685efb16e129e1c43473ead05490d678d
Palmer Dabbelt <palmer@dabbelt.com>
2014-03-27 13:38:18 -0700

    Update vcddiff to 0.0.3

--- vcddiff.rb
+++ vcddiff.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Vcddiff < Formula
-  url      'https://github.com/palmer-dabbelt/vcddiff/archive/v0.0.2.tar.gz'
+  url      'https://github.com/palmer-dabbelt/vcddiff/archive/v0.0.3.tar.gz'
   homepage 'https://github.com/palmer-dabbelt/vcddiff'
-  sha1     '519342bd76400adcba1855dc5b731179a67f0e39'
+  sha1     'a96b76c7549a8089d8fb05d1acfd309631ebf480'
 
   depends_on 'pkg-config' => :build
   depends_on 'palmer-dabbelt/palmer/pconfigure' => :build
