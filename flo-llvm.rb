b3e15ea54a7b530d83b278fcf2ff230bb8ebb20a
Palmer Dabbelt <palmer@dabbelt.com>
2014-03-27 22:05:01 -0700

    Wrong flo-llvm class name

--- flo-llvm.rb
+++ flo-llvm.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Libflo < Formula
+class FloLlvm < Formula
   url      'https://github.com/palmer-dabbelt/flo-llvm/archive/v0.0.2.tar.gz'
   homepage 'https://github.com/palmer-dabbelt/flo-llvm'
   sha1     'bdf324a6cb8c0d3b8ba81da073ad96123abb9bb8'
