e3e24a42d55445017a011d81969fe713318a00e8
Palmer Dabbelt <palmer@dabbelt.com>
2014-05-09 16:35:15 -0700

    Add flo-mwe sha1

--- flo-mwe.rb
+++ flo-mwe.rb
@@ -3,6 +3,7 @@ require 'formula'
 class FloMwe < Formula
   url      'https://github.com/palmer-dabbelt/flo-mwe/archive/v0.0.2.tar.gz'
   homepage 'https://github.com/palmer-dabbelt/flo-mwe'
+  sha1     'acc7cc86fd4c5ced2bd7978d1ec6132e0424c8c6'
 
   depends_on 'pkg-config' => :build
   depends_on 'pconfigure' => :build
