a1ad7b5bb52d6ea79f1918026947bf8d69166bc1
Palmer Dabbelt <palmer@dabbelt.com>
2014-05-12 08:44:20 -0700

    Update flo-llvm to 0.0.9

--- flo-llvm.rb
+++ flo-llvm.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class FloLlvm < Formula
-  url      'https://github.com/palmer-dabbelt/flo-llvm/archive/v0.0.8.tar.gz'
+  url      'https://github.com/palmer-dabbelt/flo-llvm/archive/v0.0.9.tar.gz'
   homepage 'https://github.com/palmer-dabbelt/flo-llvm'
-  sha1     'a7078e9e3a3a186d0ae4ba4327f79fd267f3938b'
+  sha1     '82bcebe65e862425d72a440aa8135a9b8811853f'
 
   depends_on 'pkg-config' => :build
   depends_on 'pconfigure' => :build
