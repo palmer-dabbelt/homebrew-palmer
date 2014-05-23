b46a60c2db01e84ffcc21af847842e9c7d0a50e4
Palmer Dabbelt <palmer@dabbelt.com>
2014-05-22 18:26:13 -0700

    Name mangling rules

--- vcd2step.rb
+++ vcd2step.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Vcd2Step < Formula
+class Vcd2step < Formula
   url      'https://github.com/palmer-dabbelt/vcd2step/archive/v0.0.1.tar.gz'
   homepage 'https://github.com/palmer-dabbelt/vcd2step'
   sha1     '2e44ffa0223ea0ceaebe2c9eb897ac3724b92cce'
