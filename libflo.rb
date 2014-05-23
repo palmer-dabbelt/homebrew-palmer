e358bc775446d853896b855a3f0717f304f39cc3
Palmer Dabbelt <palmer@dabbelt.com>
2014-05-22 17:48:20 -0700

    Update libflo to 0.0.15

--- libflo.rb
+++ libflo.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Libflo < Formula
-  url      'https://github.com/palmer-dabbelt/libflo/archive/v0.0.14.tar.gz'
+  url      'https://github.com/palmer-dabbelt/libflo/archive/v0.0.15.tar.gz'
   homepage 'https://github.com/palmer-dabbelt/libflo'
-  sha1     '75b28635e2b31f27c69382572a881593088dc16e'
+  sha1     'c8908fa2ac90aa9e5c1f96fe5d54c5daaa6cacc9'
 
   depends_on 'pkg-config' => :build
   depends_on 'pconfigure' => :build
