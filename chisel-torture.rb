c9671912d5cab40723b1f12232e1a27ac4392332
Palmer Dabbelt <palmer@dabbelt.com>
2014-07-09 16:07:17 -0700

    Upgrade chisel-torture to 0.0.2

--- chisel-torture.rb
+++ chisel-torture.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class ChiselTorture < Formula
-  url      'https://github.com/palmer-dabbelt/chisel-torture/archive/v0.0.1.tar.gz'
+  url      'https://github.com/palmer-dabbelt/chisel-torture/archive/v0.0.2.tar.gz'
   homepage 'https://github.com/palmer-dabbelt/chisel-torture'
-  sha1     '5f0ac3a94bc69e831fbc0e36ce948f34267710e2'
+  sha1     '1aa33e73f897072b58c48186790320547e3f94f6'
 
   depends_on 'pkg-config' => :build
   depends_on 'pconfigure' => :build
