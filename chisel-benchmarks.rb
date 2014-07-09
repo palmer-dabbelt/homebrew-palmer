950665bb3d79134a1dc9339a6e749bfbb7f28325
Palmer Dabbelt <palmer@dabbelt.com>
2014-07-09 16:01:33 -0700

    Update chisel-benchmarks

--- chisel-benchmarks.rb
+++ chisel-benchmarks.rb
@@ -1,13 +1,12 @@
 require 'formula'
 
 class ChiselBenchmarks < Formula
-  url      'https://github.com/palmer-dabbelt/chisel-benchmarks/archive/v20140623.3.tar.gz'
+  url      'https://github.com/palmer-dabbelt/chisel-benchmarks/archive/v20140709.0.tar.gz'
   homepage 'https://github.com/palmer-dabbelt/chisel-benchmarks'
-  sha1     '7d9a373754e3542500fc953f73d6b51083cbb73f'
+  sha1     '356772b48b6470cc44a02b1cca5d6b79aa72c0d5'
 
   depends_on 'pkg-config' => :build
   depends_on 'pconfigure' => :build
-  depends_on 'sbt' => :build
 
   def install
     cl = File.open("Configfile.local", "w")
