1e4b1761cc89584b582b1ba0f349f92fe56b2d4b
Palmer Dabbelt <palmer@dabbelt.com>
2014-08-21 17:14:02 -0700

    Add my gitdate at 0.0.1

---
+++ gitdate.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class Tek < Formula
+  url      'https://github.com/palmer-dabbelt/gitdate/archive/v0.0.1.tar.gz'
+  homepage 'https://github.com/palmer-dabbelt/gitdate'
+  sha1     'e0f422ad9f454aecb5c85d013e09c5aac2a7e7ff'
+
+  depends_on 'pkg-config' => :build
+  depends_on 'pconfigure' => :build
+
+  def install
+    cl = File.open("Configfile.local", "w")
+    cl.puts("PREFIX = #{prefix}")
+    cl.close
+
+    system "pconfigure"
+    system "make"
+    system "make", "install"
+  end
+end
