d5158530ac3924ce87bbcf46e894705421d32c09
Palmer Dabbelt <palmer@dabbelt.com>
2014-08-21 17:14:48 -0700

    Forgot the class, again

--- gitdate.rb
+++ gitdate.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Tek < Formula
+class Gitdate < Formula
   url      'https://github.com/palmer-dabbelt/gitdate/archive/v0.0.1.tar.gz'
   homepage 'https://github.com/palmer-dabbelt/gitdate'
   sha1     'e0f422ad9f454aecb5c85d013e09c5aac2a7e7ff'
