0bc7441606549587b45126de75fb402c098a1767
Palmer Dabbelt <palmer@dabbelt.com>
2015-02-09 17:20:07 -0800

    Update tek to 0.8.0
    This makes the inkscape dependency slightly less optional, so I just
    install it all the time now.

--- tek.rb
+++ tek.rb
@@ -1,12 +1,13 @@
 require 'formula'
 
 class Tek < Formula
-  url      'https://github.com/palmer-dabbelt/tek/archive/v0.7.0.tar.gz'
+  url      'https://github.com/palmer-dabbelt/tek/archive/v0.8.0.tar.gz'
   homepage 'https://github.com/palmer-dabbelt/tek'
-  sha1     '5423a77c531bba292e70e3f0fb9d4353231c1f64'
+  sha1     '5c6fb82d99515a4a788351d1c4fb65d1467a67d3'
 
   depends_on 'pkg-config' => :build
   depends_on 'pconfigure' => :build
+  depends_on 'inkscape'
 
   def install
     cl = File.open("Configfile.local", "w")
