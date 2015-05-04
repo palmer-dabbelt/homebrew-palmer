22cd6f13eb348b9527b787cc9355f70dcd4a9b85
Palmer Dabbelt <palmer@dabbelt.com>
2015-05-04 10:57:05 -0700

    Screwed up a SHA1

--- libflo.rb
+++ libflo.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Libflo < Formula
   url      'https://github.com/palmer-dabbelt/libflo/archive/v0.0.21.tar.gz'
   homepage 'https://github.com/palmer-dabbelt/libflo'
-  sha1     'd70e0ecf0a1e6154c0cca92828ecd078b0e88ed4'
+  sha1     'be55b519d77cbf9e12015c8a63d59688878b35a2'
 
   depends_on 'pkg-config' => :build
   depends_on 'pconfigure' => :build
