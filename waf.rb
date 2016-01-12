d286c3d112f48a682e9982c60d250989ee226f4f
Palmer Dabbelt <palmer@dabbelt.com>
2016-01-11 22:37:52 -0800

    no waf md5

--- waf.rb
+++ waf.rb
@@ -4,7 +4,6 @@ class Waf <Formula
   url 'http://waf.googlecode.com/files/waf-1.6.2'
   version '1.6.2'
   homepage 'http://code.google.com/p/waf/'
-  md5 'e8742588ca0b580b5d91eb2c8ec5786b'
 
   depends_on 'python'
 
